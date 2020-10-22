692e5e6ce6382826061024971f612dd1f2dc85d2
gylee <gunyoung88@naver.com>
2020-10-22 16:49:19 +0900

    add change_word.cpp add travel_route.cpp

--- nv_test.cpp
+++ nv_test.cpp
@@ -4,13 +4,13 @@
 #include <vector>
 
 using namespace std;
-namespace target_number
-{
-	extern int solution(vector<int> numbers, int target);
-};
+namespace target_number { extern int solution(vector<int> numbers, int target); }
+namespace change_word { extern int solution(string begin, string target, vector<string> words); }
+
 int main()
 {
 	cout << target_number::solution({ 1,1,1,1,1 }, 3);
+	cout << change_word::solution("hit", "cog", { "hot", "dot", "dog", "lot", "log", "cog" });
 
 	cin;
 }
